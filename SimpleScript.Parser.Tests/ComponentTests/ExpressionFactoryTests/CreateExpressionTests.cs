a3f9283d35e9cd89278f6d685fcf90d7b9db8968
Tim Kempkens <Tim.Kempkens@googlemail.com>
2024-06-26 06:03:04 +0200

    Added more Expression Tests

--- SimpleScript.Parser.Tests/ComponentTests/ExpressionFactoryTests/CreateExpressionTests.cs
+++ SimpleScript.Parser.Tests/ComponentTests/ExpressionFactoryTests/CreateExpressionTests.cs
@@ -2,6 +2,7 @@
 using SimpleScript.Lexer;
 using SimpleScript.Parser.NodeFactories;
 using SimpleScript.Parser.Nodes;
+using SimpleScript.Parser.Nodes.Interfaces;
 using SimpleScript.Parser.Tests.Helper;
 using SimpleScript.Parser.Tests.Helper.Factories;
 using TF = SimpleScript.Parser.Tests.Helper.Factories.TokenFactory;
@@ -83,6 +84,60 @@ namespace SimpleScript.Parser.Tests.ComponentTests.ExpressionFactoryTests
             VariableNode variableNode = TH.ConvertTo<VariableNode>(mulNode.ChildNodes[1]);
             numberNode.Value.Should().Be(43);
             variableNode.Name.Should().Be("name");
+        }
+
+        [Fact]
+        public void ShouldCreateCorrectExpressionTree_Given2AddAndMulOperation()
+        {
+            List<Token> inputTokens = [TF.Num(1), TF.Add(), TF.Num(3), TF.Mul(), TF.Var("name"), TF.Add(), TF.Num(2)];
+            IExpression result = ErrorHelper.AssertResultSuccess(_sut.Create(inputTokens));
+            (NumberNode? firstAddChild1, AddNode? firstAddChild2) = AssertAddNode<NumberNode, AddNode>(result);
+            firstAddChild1.Value.Should().Be(1);
+            (MultiplyNode? secondAddChild1, NumberNode? secondAddChild2) = AssertAddNode<MultiplyNode, NumberNode>(firstAddChild2);
+            secondAddChild2.Value.Should().Be(2);
+            (NumberNode? firstMulChild1, VariableNode? firstMulChild2) = AssertMultiplyNode<NumberNode, VariableNode>(secondAddChild1);
+            firstMulChild1.Value.Should().Be(3);
+            firstMulChild2.Name.Should().Be("name");
+        }
+
+        [Fact]
+        public void ShouldCreateCorrectExpressionTree_Given2AddAnd2MulOperation()
+        {
+            List<Token> inputTokens = [TF.Num(1), TF.Mul(), TF.Num(2), TF.Mul(), TF.Num(3), TF.Add(), TF.Num(4), TF.Add(), TF.Num(5)];
+            IExpression result = ErrorHelper.AssertResultSuccess(_sut.Create(inputTokens));
+            (MultiplyNode? firstAddChild1, AddNode? firstAddChild2) = AssertAddNode<MultiplyNode, AddNode>(result);
+            (NumberNode? firstMulChild1, MultiplyNode? firstMulChild2) = AssertMultiplyNode<NumberNode, MultiplyNode>(firstAddChild1);
+            firstMulChild1.Value.Should().Be(1);
+            (NumberNode? secondMulChild1, NumberNode? secondMulChild2) = AssertMultiplyNode<NumberNode, NumberNode>(firstMulChild2);
+            secondMulChild1.Value.Should().Be(2);
+            secondMulChild2.Value.Should().Be(3);
+            (NumberNode? secondAddChild1, NumberNode? secondAddChild2) = AssertAddNode<NumberNode, NumberNode>(firstAddChild2);
+            secondAddChild1.Value.Should().Be(4);
+            secondAddChild2.Value.Should().Be(5);
+        }
+
+        private void AssertNumberNode(IAddable expression, int value)
+        {
+            NumberNode numberNode = TH.ConvertTo<NumberNode>(expression);
+            numberNode.Value.Should().Be(value);
+        }
+
+        private (TFirstChildNode, TSecondChildNode) AssertAddNode<TFirstChildNode, TSecondChildNode>(IExpression expression)
+        {
+            AddNode addNode = TH.ConvertTo<AddNode>(expression);
+            addNode.ChildNodes.Count.Should().Be(2);
+            TFirstChildNode? firstChild = TH.ConvertTo<TFirstChildNode>(addNode.ChildNodes[0]);
+            TSecondChildNode? secondChild = TH.ConvertTo<TSecondChildNode>(addNode.ChildNodes[1]);
+            return (firstChild, secondChild);
+        }
+
+        private (TFirstChildNode, TSecondChildNode) AssertMultiplyNode<TFirstChildNode, TSecondChildNode>(IExpression expression)
+        {
+            MultiplyNode addNode = TH.ConvertTo<MultiplyNode>(expression);
+            addNode.ChildNodes.Count.Should().Be(2);
+            TFirstChildNode? firstChild = TH.ConvertTo<TFirstChildNode>(addNode.ChildNodes[0]);
+            TSecondChildNode? secondChild = TH.ConvertTo<TSecondChildNode>(addNode.ChildNodes[1]);
+            return (firstChild, secondChild);
         }
     }
 }
