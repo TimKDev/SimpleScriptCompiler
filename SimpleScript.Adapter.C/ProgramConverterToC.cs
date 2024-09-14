25f586c80999a3e569fa0f55ed7c2daacd1b856a
Tim Kempkens <tim.kempkens@googlemail.com>
2024-09-14 19:58:23 +0200

    more function work

--- SimpleScript.Adapter.C/ProgramConverterToC.cs
+++ SimpleScript.Adapter.C/ProgramConverterToC.cs
@@ -7,9 +7,34 @@ namespace SimpleScript.Adapter.C
     public class ProgramConverterToC : IConverter
     {
         private static readonly string MainBodyTemplateVariable = "mainBody";
+        private static readonly string FunctionDeclarationTemplateVariable = "functionDeclaration";
         private static readonly string MainCTemplate = @$"
                 #include <stdio.h>
                 #include <string.h>
+                #include <stdlib.h>
+                typedef struct Node {{
+                    void *data;
+                    struct Node *next;
+                }} Node;
+                Node *head = NULL;
+                void add_to_list(void *data) {{
+                    Node *new_node = (Node *)malloc(sizeof(Node));
+                    new_node->data = data;
+                    new_node->next = head;
+                    head = new_node;
+                }}
+                void free_list() {{
+                    Node *current = head;
+                    Node *next_node;
+                    while (current != NULL) {{
+                        next_node = current->next;
+                        free(current->data);
+                        free(current);
+                        current = next_node;
+                    }}
+                    head = NULL;
+                }}
+                {{{FunctionDeclarationTemplateVariable}}}
                 int main() {{
                     {{{MainBodyTemplateVariable}}}
                     return 0;
@@ -18,7 +43,7 @@ namespace SimpleScript.Adapter.C
 
         public Result<string> ConvertToCCode(ProgramNode programNode)
         {
-            var convertionResult = ConvertBodyNodeToC.ConvertToStatements(programNode.Body);
+            Result<(List<string> mainStatements, List<string> cFunctionDeclarations)> convertionResult = ConvertBodyNodeToC.ConvertToStatements(programNode.Body);
 
             if (!convertionResult.IsSuccess)
             {
@@ -27,7 +52,9 @@ namespace SimpleScript.Adapter.C
 
             (List<string> cMainScopeStatements, List<string> cFunctionDeclaration) = convertionResult.Value;
 
-            return ReplaceTemplateVariable(MainCTemplate, MainBodyTemplateVariable, string.Join('\n', cMainScopeStatements));
+            string stringWithReplacedBody = ReplaceTemplateVariable(MainCTemplate, MainBodyTemplateVariable, string.Join('\n', cMainScopeStatements));
+
+            return ReplaceTemplateVariable(stringWithReplacedBody, FunctionDeclarationTemplateVariable, string.Join('\n', cFunctionDeclaration));
         }
 
 
