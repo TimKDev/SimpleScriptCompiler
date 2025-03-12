6becc3c68b4c6887407994a74d0b77ec3af78402
Tim Kempkens <tim.kempkens@googlemail.com>
2025-03-12 20:22:31 +0100

    Deleted test project

--- SimpleScript.Lexer.OOP/IToken.cs
+++
@@ -1,7 +0,0 @@
-namespace SimpleScript.Lexer.OOP;
-
-public interface IToken
-{
-    TokenType TokenType { get; }
-    int Line { get; }
-}
--- SimpleScript.Lexer.OOP/TokenizedProgram.cs
+++
@@ -1,9 +0,0 @@
-namespace SimpleScript.Lexer.OOP;
-
-public class TokenizedProgram()
-{
-    private readonly List<IToken> _tokens = [];
-
-    public IReadOnlyList<IToken> Tokens => _tokens.AsReadOnly();
-    public void AddTokens(List<IToken> tokens) => _tokens.AddRange(tokens);
-}
