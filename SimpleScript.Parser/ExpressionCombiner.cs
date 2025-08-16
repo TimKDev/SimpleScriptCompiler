e73ce2b56625c7b308f30e016bcbb35342f45450
Tim Kempkens <tim.kempkens@googlemail.com>
2025-08-16 09:15:40 +0200

    some more work

--- SimpleScript.Parser/ExpressionCombiner.cs
+++ SimpleScript.Parser/ExpressionCombiner.cs
@@ -11,18 +11,104 @@ public class ExpressionCombiner : IExpressionCombiner
         TokenType.While
     ];
 
+    private readonly TokenType[] _binaryOperatorTypes =
+    [
+        TokenType.Plus,
+        TokenType.Multiply,
+        TokenType.Equal,
+        TokenType.NotEqual,
+        TokenType.GreaterOrEqual,
+        TokenType.SmallerOrEqual,
+        TokenType.Greater,
+        TokenType.Smaller,
+        //Was ist Assign?????
+    ];
+
+    private readonly TokenType[] _bracketTokenTypes =
+    [
+        TokenType.OpenBracket,
+        TokenType.ClosedBracket
+    ];
+
     public Result<List<Token>> GetExpression(List<Token> tokens, int startPosition)
     {
         var result = new List<Token>();
+        var openExpression = true;
         for (int i = startPosition; i < tokens.Count; i++)
         {
             var currentToken = tokens[i];
+
             if (_tokensToEndExpression.Contains(currentToken.TokenType))
             {
+                if (openExpression)
+                {
+                    return Error.Create("Expression is not complete");
+                }
+
                 break;
             }
 
+            if (_bracketTokenTypes.Contains(currentToken.TokenType))
+            {
+                result.Add(currentToken);
+                break;
+            }
+
+            if (_binaryOperatorTypes.Contains(currentToken.TokenType))
+            {
+                if (openExpression)
+                {
+                    return Error.Create("Expression is not complete");
+                }
+
+                result.Add(currentToken);
+                openExpression = true;
+            }
+
+            if (openExpression)
+            {
+                openExpression = false;
+
+                if (i + 2 < tokens.Count &&
+                    (tokens[i].TokenType, tokens[i + 1].TokenType) is (TokenType.Variable, TokenType.OpenBracket))
+                {
+                    var tokensAfterFirstOpenBracketOfInvocation =
+                        GetTokensOfArgumentsOfFunctionInvocation(tokens, i + 2);
+                    result.Add(tokens[i]); //Function name
+                    result.Add(tokens[i + 1]); // Open Bracket
+                    result.AddRange(tokensAfterFirstOpenBracketOfInvocation); // Rest up to closed Bracket. 
+                    i += tokensAfterFirstOpenBracketOfInvocation.Count + 1;
+                    continue;
+                }
+
+                result.Add(currentToken);
+                continue;
+            }
+
             result.Add(currentToken);
+        }
+
+        return result;
+    }
+
+    private static List<Token> GetTokensOfArgumentsOfFunctionInvocation(List<Token> tokens, int startOfExpression)
+    {
+        var result = new List<Token>();
+        var bracketScore = 1;
+        var j = startOfExpression;
+        while (j < tokens.Count && bracketScore != 0)
+        {
+            result.Add(tokens[j]);
+            if (tokens[j].TokenType == TokenType.OpenBracket)
+            {
+                bracketScore++;
+            }
+            else if (tokens[j].TokenType == TokenType.ClosedBracket)
+            {
+                bracketScore--;
+            }
+
+            j++;
         }
 
         return result;
--- SimpleScript.Parser/StatementCombiner.cs
+++ SimpleScript.Parser/StatementCombiner.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.InteropServices.JavaScript;
-using EntertainingErrors;
+﻿using EntertainingErrors;
 using SimpleScript.Lexer;
 using SimpleScript.Parser.Interfaces;
 
